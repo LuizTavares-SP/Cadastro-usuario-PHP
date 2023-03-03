4e4b530ec1e2b194069d2b90f131020279b29326
Mr-Robot-PPADS <81531056+LuizTavares-SP@users.noreply.github.com>
2023-03-03 16:33:13 -0300

    projeto

---
+++ cadastro.php
@@ -0,0 +1,19 @@
+<?php 
+
+include("conexao.php");
+
+$nome=$_POST['nome'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO usuarios(nome, email) VALUES ('$nome', '$email')";
+if(mysqli_query($conexao, $sql)){
+  echo "Usuário cadastrado com sucesso";
+} else {
+  echo "Erro".mysqli_connect_error($conexao);
+};
+
+mysqli_close($conexao);
+
+
+
+?>
